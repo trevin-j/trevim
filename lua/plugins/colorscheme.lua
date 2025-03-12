1d3f5d91994bd6aa5ff8ad114a3f15df475dcf0c
Trevin Jones <tr3vinj@gmail.com>
2025-03-11 22:16:36 -0600

    transparent bg and add session management!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -31,6 +31,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require 'cyberdream'.setup {
+      transparent = true,
+    }
     vim.cmd("colorscheme cyberdream")
   end,
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,24 @@ return {
     'nvim-telescope/telescope.nvim',
     'nvim-treesitter/nvim-treesitter',
     'lewis6991/gitsigns.nvim',
+    {
+      'folke/persistence.nvim',
+      event = "BufReadPre",
+      opts = {},
+      config = function()
+        -- load the session for the current directory
+        vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end)
+
+        -- select a session to load
+        vim.keymap.set("n", "<leader>qS", function() require("persistence").select() end)
+
+        -- load the last session
+        vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end)
+
+        -- stop Persistence => session won't be saved on exit
+        vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end)
+      end,
+    }
   },
   opts = {
     bigfile = { enabled = true },
