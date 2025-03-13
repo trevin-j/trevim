4f41f6d76b950c128505d0d08ed0c6f6dbb07f94
Trevin Jones <tr3vinj@gmail.com>
2025-03-13 16:09:49 -0600

    hop!

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,26 +1,52 @@
 return {
   'smoka7/hop.nvim',
-  branch = 'v2',
+  tag = 'v2.7.2',
   config = function()
     local hop = require('hop')
     hop.setup {
       keys = 'ntesiroaufcywxhdlp',
     }
-    local directions = require('hop.hint').HindDirection
+    -- Better f/F/t/T
+    local directions = require('hop.hint').HintDirection
     vim.keymap.set('n', 'f', function()
       hop.hint_char1 {
         direction = directions.AFTER_CURSOR,
+        current_line_only = true,
       }
     end, { desc = 'Hop to character after cursor', remap = true })
-    vim.keymap.set('n', 'F', function()
+    vim.keymap.set('', 'F', function()
       hop.hint_char1 {
         direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
       }
     end, { desc = 'Hop to character before cursor', remap = true })
-    vim.keymap.set('n', 't', function()
+    vim.keymap.set('', 't', function()
+      hop.hint_char1 {
+        direction = directions.AFTER_CURSOR,
+        current_line_only = true,
+        hint_offset = -1,
+      }
+    end, { desc = 'Hop to before character after cursor', remap = true })
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = true,
+        hint_offset = 1,
+      }
+    end, { desc = 'Hop to after character before cursor', remap = true })
+
+    -- Navigating anywhere in buffer with s/S
+    vim.keymap.set('', 's', function()
       hop.hint_char2 {
         direction = directions.AFTER_CURSOR,
+        current_line_only = false,
       }
-    end, { desc = 'Hop to two characters after cursor', remap = true })
+    end, { desc = 'Hop to character after cursor', remap = true })
+    vim.keymap.set('', 'S', function()
+      hop.hint_char2 {
+        direction = directions.BEFORE_CURSOR,
+        current_line_only = false,
+      }
+    end, { desc = 'Hop to character before cursor', remap = true })
   end,
 }
