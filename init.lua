6f3db442639a8d12e2fe6a84faddb19a6be28569
Trevin Jones <tr3vinj@gmail.com>
2025-03-14 14:39:30 -0600

    updates

--- init.lua
+++ init.lua
@@ -4,12 +4,6 @@ require("bootstrap")
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- Pres ESC to clear highlights from search
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) -- Esc twice to exit terminal mode
 
--- Nicer split nav
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- Buffer navigation with bh and bl
 vim.keymap.set('n', '<C-h>', ':bprevious<CR>', { desc = 'Go to previous buffer' })
 vim.keymap.set('n', '<C-l>', ':bnext<CR>', { desc = 'Go to next buffer' })
@@ -24,18 +18,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
--- Tmux auto rename windows!
-local uv = vim.uv
-
-vim.api.nvim_create_autocmd({ 'VimEnter', 'VimLeave' }, {
-	callback = function()
-		if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-			uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. '/tmux-window-name/scripts/rename_session_windows.py', {})
-		end
-	end,
-})
-
-
-
 
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -50,19 +50,6 @@ _/ (__/_   _/__/ (__(/_(_/__/__/ / (_
         { section = "keys", gap = 1, padding = 2 },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        -- {
-        --   icon = " ",
-        --   title = "Git Status",
-        --   section = "terminal",
-        --   enabled = function()
-        --     return Snacks.git.get_root() ~= nil
-        --   end,
-        --   cmd = "git status --short --branch --renames",
-        --   height = 5,
-        --   padding = 1,
-        --   ttl = 5 * 60,
-        --   indent = 3,
-        -- },
       }
     },
     picker = {
